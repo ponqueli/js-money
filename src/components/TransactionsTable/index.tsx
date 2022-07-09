70a974ef38301b2aee5f00688aa324db4b5405b2
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-07-09 20:34:53 -0300

    feat: creating transaction table component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+  return(
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/05/2020</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$ 12.000,00</td>
+            <td>Fixa</td>
+            <td>20/05/2020</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R$ 2.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>10/05/2020</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
