e512ae730e5fd8ff2b73979de0ab1f098f72410f
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-07-09 12:05:57 -0300

    setup: configuring style components and css global setup

---
+++ src/styles/global.ts
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f2f5;
+    --shape: #FFFFFF;
+    --red: #E52E4D;
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+    --text-title: #363f5f;
+    -text-body: #969CB3;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; //15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; //14px
+    }
+
+    @media (max-width: 320px) {
+      font-size: 75%; //12px
+    }
+  }
+
+  //REM - 1rem = 16px (tamanho do font-size da nossa página)	
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+  
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+`;
